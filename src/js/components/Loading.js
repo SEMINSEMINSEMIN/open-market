c3bfbb1284b0ce358caecf3a8a3374ea2610e160
강세민 <ksm9802@gmail.com>
2023-02-25 19:46:53 +0900

    Refactor: 로딩 인디케이터 리페인트 문제 해결 #8
    기존 코드: background-color를 바꾸는 애니메이션, background-color가 변하기 때문에 애니메이션 실행시 리페인트가 계속 일어남
    
    해결: HTML을 구조화 해 레이어를 두 개 만든 뒤 한 레이어의 opacity를 조정해 흰색과 초록색이 겹치도록 함. opacity가 변하더라도 리페인트는 발생하지 않음

--- src/js/components/Loading.js
+++ src/js/components/Loading.js
@@ -15,6 +15,10 @@ export default class Loading extends HTMLElement {
             const circle = document.createElement("div");
             circle.setAttribute("class", "f_circleG");
             circle.setAttribute("id", `frotateG_0${i}`);
+            const circleBg =  document.createElement("div");
+            circleBg.setAttribute("class", "f_circleBg");
+            circleBg.setAttribute("id", `circleBg_0${i}`);
+            circle.appendChild(circleBg);
             wrapper.appendChild(circle);
         }
 
@@ -39,174 +43,115 @@ export default class Loading extends HTMLElement {
             width: 122px;
             height: 122px;
             margin: auto;
-            transform: translateY(-50%, -50%) scale(0.6);
-                -o-transform: translate(-50%, -50%) scale(0.6);
-                -ms-transform: translate(-50%, -50%) scale(0.6);
-                -webkit-transform: translate(-50%, -50%) scale(0.6);
-                -moz-transform: translate(-50%, -50%) scale(0.6);
+            transform: translate(-50%, -50%) scale(0.6);
+
         }
         
         .f_circleG{
             position: absolute;
-            background-color: rgb(245,245,245);
+            background-color: rgba(240,240,240,0.85);
+            opacity: 1;
+            z-index: -1;
             height: 22px;
             width: 22px;
             border-radius: 11px;
-                -o-border-radius: 11px;
-                -ms-border-radius: 11px;
-                -webkit-border-radius: 11px;
-                -moz-border-radius: 11px;
+        }
+
+        .f_circleBg {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background-color: rgb(33, 191, 72);
+            opacity: 0;
+            z-index: 1;
+            border-radius: 11px;
             animation-name: f_fadeG;
-                -o-animation-name: f_fadeG;
-                -ms-animation-name: f_fadeG;
-                -webkit-animation-name: f_fadeG;
-                -moz-animation-name: f_fadeG;
             animation-duration: 1.2s;
-                -o-animation-duration: 1.2s;
-                -ms-animation-duration: 1.2s;
-                -webkit-animation-duration: 1.2s;
-                -moz-animation-duration: 1.2s;
             animation-iteration-count: infinite;
-                -o-animation-iteration-count: infinite;
-                -ms-animation-iteration-count: infinite;
-                -webkit-animation-iteration-count: infinite;
-                -moz-animation-iteration-count: infinite;
             animation-direction: normal;
-                -o-animation-direction: normal;
-                -ms-animation-direction: normal;
-                -webkit-animation-direction: normal;
-                -moz-animation-direction: normal;
         }
         
         #frotateG_01{
             left: 0;
             top: 49px;
-            animation-delay: 0.45s;
-                -o-animation-delay: 0.45s;
-                -ms-animation-delay: 0.45s;
-                -webkit-animation-delay: 0.45s;
-                -moz-animation-delay: 0.45s;
         }
         
         #frotateG_02{
             left: 14px;
             top: 14px;
-            animation-delay: 0.6s;
-                -o-animation-delay: 0.6s;
-                -ms-animation-delay: 0.6s;
-                -webkit-animation-delay: 0.6s;
-                -moz-animation-delay: 0.6s;
         }
         
         #frotateG_03{
             left: 49px;
             top: 0;
-            animation-delay: 0.75s;
-                -o-animation-delay: 0.75s;
-                -ms-animation-delay: 0.75s;
-                -webkit-animation-delay: 0.75s;
-                -moz-animation-delay: 0.75s;
         }
         
         #frotateG_04{
             right: 14px;
             top: 14px;
-            animation-delay: 0.9s;
-                -o-animation-delay: 0.9s;
-                -ms-animation-delay: 0.9s;
-                -webkit-animation-delay: 0.9s;
-                -moz-animation-delay: 0.9s;
         }
         
         #frotateG_05{
             right: 0;
             top: 49px;
-            animation-delay: 1.05s;
-                -o-animation-delay: 1.05s;
-                -ms-animation-delay: 1.05s;
-                -webkit-animation-delay: 1.05s;
-                -moz-animation-delay: 1.05s;
         }
         
         #frotateG_06{
             right: 14px;
             bottom: 14px;
-            animation-delay: 1.2s;
-                -o-animation-delay: 1.2s;
-                -ms-animation-delay: 1.2s;
-                -webkit-animation-delay: 1.2s;
-                -moz-animation-delay: 1.2s;
         }
         
         #frotateG_07{
             left: 49px;
             bottom: 0;
-            animation-delay: 1.35s;
-                -o-animation-delay: 1.35s;
-                -ms-animation-delay: 1.35s;
-                -webkit-animation-delay: 1.35s;
-                -moz-animation-delay: 1.35s;
         }
         
         #frotateG_08{
             left: 14px;
             bottom: 14px;
-            animation-delay: 1.5s;
-                -o-animation-delay: 1.5s;
-                -ms-animation-delay: 1.5s;
-                -webkit-animation-delay: 1.5s;
-                -moz-animation-delay: 1.5s;
         }
-        
-        
+
+        #circleBg_01 {
+            animation-delay: 0.45s;
+        }
+
+        #circleBg_02 {
+            animation-delay: 0.6s;
+        }
+
+        #circleBg_03 {
+            animation-delay: 0.75s;
+        }
+
+        #circleBg_04 {
+            animation-delay: 0.9s;
+        }
+
+        #circleBg_05 {
+            animation-delay: 1.05s;
+        }
+
+        #circleBg_06 {
+            animation-delay: 1.2s;
+        }
+
+        #circleBg_07 {
+            animation-delay: 1.35s;
+        }
+
+        #circleBg_08 {
+            animation-delay: 1.5s;
+        }
         
         @keyframes f_fadeG{
             0%{
-                background-color: rgb(33,191,72);
+                opacity: 1;
             }
         
             100%{
-                background-color: rgba(240,240,240,0.98);
-            }
-        }
-        
-        @-o-keyframes f_fadeG{
-            0%{
-                background-color: rgb(33,191,72);
-            }
-        
-            100%{
-                background-color: rgba(240,240,240,0.98);
-            }
-        }
-        
-        @-ms-keyframes f_fadeG{
-            0%{
-                background-color: rgb(33,191,72);
-            }
-        
-            100%{
-                background-color: rgba(240,240,240,0.98);
-            }
-        }
-        
-        @-webkit-keyframes f_fadeG{
-            0%{
-                background-color: rgb(33,191,72);
-            }
-        
-            100%{
-                background-color: rgba(240,240,240,0.98);
-            }
-        }
-        
-        @-moz-keyframes f_fadeG{
-            0%{
-                background-color: rgb(33,191,72);
-            }
-        
-            100%{
-                background-color: rgba(240,240,240,0.98);
+                opacity: 0;
             }
         }
         `;
