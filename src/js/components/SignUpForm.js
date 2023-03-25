f3e898e0931027b575be0efeb845705822613787
강세민 <ksm9802@gmail.com>
2023-03-25 13:53:35 +0900

    Feat: (회원가입 페이지 - 구매) 유효성 검사 및 아이디 중복 체크 #12

--- src/js/components/SignUpForm.js
+++ src/js/components/SignUpForm.js
@@ -1,40 +1,52 @@
 import { $ } from "../utils/querySelector.js";
+import useHttp from "../utils/useHttp.js";
 
 export default class SignUpForm {
+    isIdValid;
+    isPwValid;
+    isPwSame;
+    isPhone2Valid;
+    isPhone3Valid;
+
     constructor($main, $form) {
         this.$main = $main;
+        this.validityFuncLookUp = {
+            "pw": this.pwCheck,
+            "pwCheck": this.pwSameCheck,
+            "id": this.idCheck,
+            "phone": this.phoneCheck
+        };
         !$form && this.render($main);
     }
 
     render($main) {
-        const pwPattern = "^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{8,}$";
+        const pwPattern = "^(?=.*[a-z])(?=.*\d).{8,}$";
         const buyerHTML = `
-            <label class="id">
+            <label>
                 <p>아이디</p>
-                <input type="text" pattern="[a-zA-Z0-9]{,20}" maxlength="20" required>
-                <button class="id-check" type="button">중복확인</button>
+                <input class="formItem id" type="text" pattern="[a-zA-Z0-9]{0,20}" maxlength="20" required>
                 <p class="msg"></p>
             </label>
-            <label class="pw">
+            <label>
                 <p>비밀번호</p>
-                <input type="password" pattern=${pwPattern} minlength="8" required>
+                <input class="formItem pw" type="password" pattern=${pwPattern} minlength="8" required>
                 <p class="msg"></p>
             </label>
-            <label class="pwCheck">
+            <label>
                 <p>비밀번호 재확인</p>
-                <input type="password" pattern=${pwPattern} minlength="8" required>
+                <input class="formItem pwCheck" type="password" pattern=${pwPattern} minlength="8" required>
                 <p class="msg"></p>
             </label>
-            <label class="name">
+            <label>
                 <p>이름</p>
-                <input type="text" required>
+                <input class="formItem name" type="text" required>
                 <p class="msg"></p>
             </label>
-            <label class="phone">
+            <label>
                 <p>휴대폰번호</p>
                 <span class="first">010</span>
-                <input class="second" type="text" pattern="[0-9]{3,4}" minlength="3" maxlength="4" required>
-                <input class="third" type="text" pattern="[0-9]{4}" maxlength="4" required>
+                <input class="formItem second phone" type="text" pattern="[0-9]{3,4}" minlength="3" maxlength="4" required>
+                <input class="formItem third phone" type="text" pattern="[0-9]{4}" maxlength="4" required>
                 <p class="msg"></p>
             </label>
         `;
@@ -80,19 +92,23 @@ export default class SignUpForm {
         $seller.addEventListener("click", () => {
             this.handleSellerClicked($agreement);
         });
+
+        $signUpForm.addEventListener("focusout", (e) => {
+            this.handleInpFocusOut(e.target);
+        });
     }
 
     handleSellerClicked(insertPosition) {
         const sellerHTML = `
-            <label class="seller-num">
+            <label>
                 <p>사업자 등록번호</p>
-                <input type="text" pattern="[0-9]{10}" maxlength="10">
+                <input class="formItem seller-num" type="text" pattern="[0-9]{10}" maxlength="10">
                 <button type="button">인증</button>
                 <p class="msg"></p>
             </label>
-            <label class="store-name">
+            <label>
                 <p>스토어 이름</p>
-                <input type="text">
+                <input class="formItem store-name" type="text">
                 <p class="msg"></p>
             </label>
         `;
@@ -108,5 +124,89 @@ export default class SignUpForm {
             $sellerNum.remove();
             $storeName.remove();
         }
+    }
+
+    handleInpFocusOut(target) {
+        if (!target.classList.contains("formItem")) {
+            return;
+        }
+
+        const {validity, classList, parentElement} = target;
+        const type = classList[classList.length - 1];
+        const $msg = parentElement.lastElementChild;
+
+        if (validity.valueMissing) {
+            $msg.textContent = "필수 정보입니다.";
+        } else {
+            const callback = this.validityFuncLookUp[type];
+            if (callback) {
+                this.handleValidityCheck(callback, $msg, {validity, classList, target, parentElement});
+            } else $msg.textContent = "";
+        }
+    }
+
+    handleValidityCheck(callback, $msg, targetInfo) {
+        const bounceCallback = callback.bind(this);
+        bounceCallback($msg, targetInfo);
+    }
+
+    pwCheck($msg, targetInfo) {
+        const {target} = targetInfo;
+        const isValid = /^(?=.*[a-z])(?=.*\d).{8,}$/.test(target.value);
+        if (isValid) {
+            $msg.textContent = "";
+            this.isPwValid = true;
+        } else {
+            $msg.textContent = "비밀번호는 8자 이상이어야 하며, 영소문자와 숫자가 각각 한 개 이상 필수적으로 들어가야 합니다.";
+            this.isPwValid = false;
+        }
+    }
+
+    pwSameCheck($msg, targetInfo) {
+        const {target, parentElement} = targetInfo;
+        const $prevInput = $(".pw", parentElement.previousElementSibling);
+        if ($prevInput.value === target.value) {
+            $msg.textContent = "";
+            this.isPwSame = true;
+        } else {
+            $msg.textContent = "비밀번호가 일치하지 않습니다.";
+            this.isPwSame = false;
+        }
+    }
+
+    idCheck($msg, targetInfo) {
+        const {validity, target} = targetInfo;
+        if (!validity.valid) {
+            $msg.focus();
+            $msg.textContent = "20자 이내의 영문 소문자, 대문자, 숫자만 사용 가능합니다.";
+            this.isIdValid = false;
+        } else {
+            this.idUniqCheck(target, $msg);
+        }
+    }
+
+    phoneCheck($msg, targetInfo) {
+        const {classList, validity} = targetInfo;
+        if (classList.contains("second")) {
+            this.isPhone2Valid = validity.valid;
+        } else if (classList.contains("third")) {
+            this.isPhone3Valid = validity.valid;
+        }
+        $msg.textContent = validity.valid && this.isPhone2Valid === true && this.isPhone3Valid === true 
+            ? "" 
+            : "핸드폰번호는 010으로 시작하는 10~11자리 숫자여야 합니다.";
+    }
+
+    async idUniqCheck(target, changeTarget) {
+        const reqConfig = {
+            method: "POST",
+            url: "/accounts/signup/valid/username/",
+            body: {"username": target.value}
+        };
+
+        await useHttp(reqConfig, (data, resStatus) => {
+            changeTarget.textContent = data["FAIL_Message"] || data["Success"];
+            this.isIdValid = resStatus !== 400;
+        });
     }
 }
